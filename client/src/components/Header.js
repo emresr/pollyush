5fdd841935d146f5f27b1539e04f39870343db64
emre <emre>
2020-11-02 23:29:33 +0200

    components

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,7 +3,8 @@ import React, { useState } from "react";
 const Header = () => {
 	return (
 		<div className="header-main">
-			<h1 classname="header-title">Polly </h1>
+			<h1 className="text-center mt-5">Polly</h1>
+			<h3 className="text-center mb-4">A poll app built with React </h3>
 		</div>
 	);
 };

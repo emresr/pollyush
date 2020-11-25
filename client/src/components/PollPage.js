bee54ac2b82353f37254b25e437bccb312d2fb94
emre <emre>
2020-11-25 22:58:05 +0300

    css

--- client/src/components/PollPage.js
+++ client/src/components/PollPage.js
@@ -31,7 +31,32 @@ console.log(polls)
 	const pollpage = (
       <div className="container">
 		<div className="row">
-			<div className="col-sm ">
+			<div className="col-sm">
+				<div className="pollpage">
+					<h1 className="pollpage-title">Whats your favor?</h1>
+					<h3>Toplam Oy Sayısı:300</h3>
+					<div>
+						<h5>Pizza - %50</h5>
+						<h5>Hamburger - %50</h5>
+						<h5>Lahmacun - %50</h5>
+						<h5>HotDog - %50</h5>
+					</div>
+				</div>
+            </div>
+            
+				<div className="col-sm chart">
+					<PieChart
+						viewBoxSize="[30,30]"
+						data={[
+							{ title: "One", value: 10, color: "#E38627" },
+							{ title: "Two", value: 15, color: "#C13C37" },
+							{ title: "Three", value: 20, color: "#6A2135" },
+						]}
+					/>
+				</div>
+			
+	   {/*
+            <div className="col-sm ">
 				<div className="poll-infos">
 					<h1 className="infos-title">{polls.title}</h1>
 					<h3>Toplam Oy Sayısı:300</h3>
@@ -40,6 +65,7 @@ console.log(polls)
 					</div>
 				</div>
             </div>
+            
 				<div className="col-sm">
 					<PieChart
 						viewBoxSize="[10,10]"
@@ -50,6 +76,7 @@ console.log(polls)
 						]}
 					/>
 				</div>
+      */}
 			
 		</div>
 	  </div>

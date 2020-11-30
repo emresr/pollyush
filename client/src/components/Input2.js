82580b538535bc89fb90b062f30d9b4051e8b929
emre <emre>
2020-11-30 21:03:44 +0300

    input component

--- client/src/components/Input2.js
+++ client/src/components/Input2.js
@@ -1,28 +1,67 @@
-import React, { useState } from "react";
+import React, { useState,Component } from "react";
 
-function Input2() {
-  const [inputList, setInputList] = useState([{ firstName: "", lastName: "" }]);
+class Input2 extends Component {
 
-  return (
-    <div className="App">
-      {inputList.map((x, i) => {
-        return (
-          <div className="box">
-            <input
-              name="firstName"
-              value={x.firstName}
-            />
-            
-            <div className="btn-box">
-              {inputList.length !== 1 && <button className="mr10">Remove</button>}
-              {inputList.length - 1 === i && <button>Add</button>}
-            </div>
-          </div>
-        );
-      })}
-      <div style={{ marginTop: 20 }}>{JSON.stringify(inputList)}</div>
-    </div>
-  );
-}
+
+   state = {
+         options:[]
+   }
+
+
+   addOption(){
+    this.setState({options:[...this.state.options,"" ]}
+      )
+   }
+
+   handleChange(e,index){
+      this.state.options[index] = e.target.value
+
+      //set changed state...
+      this.setState({options:this.state.options})
+   }
+
+    handleRemove(index){
+      this.state.options.splice(index,1)
+
+      console.log(this.state.options,'$$$$')
+
+      //update state
+
+      this.setState({options:this.state.options})
+    }
+
+   handleSubmit(e){
+    console.log(this.state,"ew")
+   }
+
+render() { 
+     return (
+
+        <div className="input">
+         <label> Title </label>
+
+         {
+          this.state.options.map((option,index) => {
+            return(
+               <div key ={index}>
+                   <input onChange={(e)=>this.handleChange(e)} value={option} />
+                   <button onClick={()=>this.handleRemove(index)}>  </button>
+               </div>
+            )
+          })
+         }
+         <hr />
+
+         <button onClick={(e)=>this.addOption(e)}> Add Option </button>
+
+         <hr />
+         
+         <button onClick={(e)=>this.handleSubmit()}> Submit </button>
+
+        </div>
+
+     	);
+   }
+ }
 
 export default Input2;
--- client/src/components/Mainpage.js
+++ client/src/components/Mainpage.js
@@ -1,24 +1,21 @@
-import React, { Fragment } from "react"
-import Input from "./Input.js"
-import List from "./List.js"
+import React, { Fragment } from "react";
+import Input from "./Input.js";
+import List from "./List.js";
 
-import Input2 from "./Input2.js"
-
-
+import Input2 from "./Input2.js";
 
 const Mainpage = () => {
 	return (
 		<>
-		<div className="container">
-		 <div className="row">
-				<Input />
-				<Input2/>
-				<List />
-
-			</div>
+			<div className="container">
+				<div className="row">
+					<Input />
+					<Input2 />
+					<List />
+				</div>
 			</div>
 		</>
-	)
-}
+	);
+};
 
-export default Mainpage
+export default Mainpage;

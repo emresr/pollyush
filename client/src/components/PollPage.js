521855ab0dc6aa684b7f552555803d6735e64c97
emresr <emresurbahan@yahoo.com>
2020-12-12 22:19:47 +0200

    firebase added

--- client/src/components/PollPage.js
+++ client/src/components/PollPage.js
@@ -33,6 +33,21 @@ const PollPage = () => {
 
 	console.log(polls.option);
 
+function copy() {
+  /* Get the text field */
+  var copyText = document.getElementById("myInput");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+
+  /* Alert the copied text */
+  alert("Copied the text: " + copyText.value);
+}
+
 	const k = [
 		{
 			title: "Whats your favor?",
@@ -69,12 +84,18 @@ const PollPage = () => {
 							Result
 						</Link>
 
+<input type="text" value="Hello World" id="myInput"/>
 
+<button onclick="copy()">Copy text</button>
 
 						<h1>Share On Twitter </h1>
 						<h1>Share On Dis </h1>
 					</div>
+
 				</div>
+
+
+				
 				{/*
 
 						{polls.map(	polls => (
